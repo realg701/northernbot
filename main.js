e31df8478d904cd9256b7c93a7bed8df10d302d9
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-06-25 04:34:48 +0500

    ui

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     var scroll = $(window).scrollTop();
     if (scroll > 10) {
       $("nav").css("background", "#64c6ff");
-      $("a").css("color", "black");
+      //   $("a").css("color", "black");
     } else {
       $("nav").css("background", "#ffffff88");
       // $("a").css("color", "blue");
@@ -42,32 +42,35 @@ $("nav").ready(function () {
 // Footer
 $("footer").ready(function () {
   $("footer").append(`<!-- Section: Social media -->
-<section class="d-flex justify-content-between p-4" style="background-color:#64c6ff;color:black">
-    <!-- Left -->
-    <div class="me-5">
-        <span>Get connected with us on social networks:</span>
-    </div>
-    <!-- Left -->
+<section class="p-4" style="background-color:#64c6ff;color:black">
+    <div class="d-flex justify-content-center justify-content-md-between
+ align-items-baseline mx-auto" style="max-width: 1200px;">
+        <!-- Left -->
+        <div class="d-none d-md-block social-text">
+            <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
 
-    <!-- Right -->
-    <div class="social-links">
-        <a href="" class="text-white me-4">
-            <i class="fab fa-facebook-f" style="color:black"></i>
-        </a>
-        <a href="" class="text-white me-4">
-            <i class="fab fa-twitter" style="color:black"></i>
-        </a>
-        <a href="" class="text-white me-4">
-            <i class="fab fa-google"></i>
-        </a>
-        <a href="" class="text-white me-4">
-            <i class="fab fa-instagram"></i>
-        </a>
-        <a href="" class="text-white me-4">
-            <i class="fab fa-linkedin"></i>
-        </a>
+        <!-- Right -->
+        <div class="social-links">
+            <a href="" class="text-white me-4">
+                <i class="fab fa-facebook-f" style="color:black"></i>
+            </a>
+            <a href="" class="text-white me-4">
+                <i class="fab fa-twitter" style="color:black"></i>
+            </a>
+            <a href="" class="text-white me-4">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="text-white me-4">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="text-white me-4">
+                <i class="fab fa-linkedin"></i>
+            </a>
+        </div>
+        <!-- Right -->
     </div>
-    <!-- Right -->
 </section>
 <!-- Section: Social media -->
 
@@ -80,12 +83,10 @@ $("footer").ready(function () {
             <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <!-- Content -->
                 <a href="${preFix}/"><h6 class="text-uppercase fw-bold text-white">NorthernBot</h6></a>
-                <hr class="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: #7c4dff; height: 2px" />
+                <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
                 <p>
-                    Here you can use rows and columns to organize your footer
-                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.
+                    Here you can use rows and columns to organize your footer content. Lorem ipsum
+                    dolor sit amet, consectetur adipisicing elit.
                 </p>
             </div>
             <!-- Grid column -->
@@ -112,27 +113,6 @@ $("footer").ready(function () {
             <!-- Grid column -->
 
             <!-- Grid column -->
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold">Useful links</h6>
-                <hr class="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: #7c4dff; height: 2px" />
-                <p>
-                    <a href="#!" class="text-white">Your Account</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Become an Affiliate</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Shipping Rates</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Help</a>
-                </p>
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                 <!-- Links -->
                 <a href="${preFix}/pages/contact"><h6 class="text-uppercase fw-bold text-white">Contact</h6></a>
@@ -141,7 +121,6 @@ $("footer").ready(function () {
                 <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
                 <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
                 <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
             </div>
             <!-- Grid column -->
         </div>
--- pages/products/data.js
+++ pages/products/data.js
@@ -4,23 +4,39 @@ export const data = [
     title: "Walk Behind Floor Scrubber SJ45",
     category: "Floor Scrubber",
     image: "./mini-ride-on-floor-xcrubber-SJ70/images/image-1.jpeg",
+    features:
+      "Product feature 1. The operation panel is simple, easy to understand and easy to use. 2. Special water tank forward-tilt lock design makes safer working. 3. Rotational molded brush plate cover, sturdy and durable. 4. The dual design of liquid level switch and filter cotton protects the motor and increases its service life. 5. Hand-push battery floor scrubber, easy to operate. 6. Lead-acid maintenance-free battery ensures continuous working hours. 7. Use fur skirt instead of traditional rubber skirt for longer service life.",
   },
   {
     link: "/pages/products/mini-ride-on-floor-xcrubber-SJ70",
     title: "Mini Ride On Floor Scrubber SJ70",
     category: "Floor Scrubber",
     image: "./mini-ride-on-floor-xcrubber-SJ70/images/image-2.jpeg",
+    features:
+      "Product feature 1. Silent floor washing machine to meet the needs of special occasions. 2. Built-in car charger, anti-lost, anti-fall and anti-water intrusion. 3. The squeegee is easy to assemble and disassemble, and the operation is simple and easy to learn. 4. Turning radius 1.7 meters, which can turn around in place. 5. Triple anti-water intrusion devices: waterproof cover, floating ball devices and liquid level sensors. 6. Design of two water injection ports: the large water injection port can achieve rapid water injection, and the smallone is suitable for places far away from the water source. 7. Lead-acid maintenance-free battery ensures continuous working hours.",
   },
   {
     link: "/pages/products/walk-behind-floor-scrubber-SJ45",
     title: "Walk Behind Floor Scrubber SJ45",
     category: "Floor Scrubber",
     image: "./mini-ride-on-floor-xcrubber-SJ70/images/image-1.jpeg",
+    features:
+      "Product feature 1. The operation panel is simple, easy to understand and easy to use. 2. Special water tank forward-tilt lock design makes safer working. 3. Rotational molded brush plate cover, sturdy and durable. 4. The dual design of liquid level switch and filter cotton protects the motor and increases its service life. 5. Hand-push battery floor scrubber, easy to operate. 6. Lead-acid maintenance-free battery ensures continuous working hours. 7. Use fur skirt instead of traditional rubber skirt for longer service life.",
+  },
+  {
+    link: "/pages/products/walk-behind-floor-scrubber-NB01",
+    title: "Walk Behind Floor Scrubber NB01",
+    category: "Floor Scrubber",
+    image: "./mini-ride-on-floor-xcrubber-SJ70/images/image-1.jpeg",
+    features:
+      "Product feature 1. The operation panel is simple, easy to understand and easy to use. 2. Special water tank forward-tilt lock design makes safer working. 3. Rotational molded brush plate cover, sturdy and durable. 4. The dual design of liquid level switch and filter cotton protects the motor and increases its service life. 5. Hand-push battery floor scrubber, easy to operate. 6. Lead-acid maintenance-free battery ensures continuous working hours. 7. Use fur skirt instead of traditional rubber skirt for longer service life.",
   },
   {
     link: "/pages/products/mini-ride-on-floor-xcrubber-SJ70",
     title: "Mini Ride On Floor Scrubber SJ70",
     category: "Floor Scrubber",
     image: "./mini-ride-on-floor-xcrubber-SJ70/images/image-2.jpeg",
+    features:
+      "Product feature 1. Silent floor washing machine to meet the needs of special occasions. 2. Built-in car charger, anti-lost, anti-fall and anti-water intrusion. 3. The squeegee is easy to assemble and disassemble, and the operation is simple and easy to learn. 4. Turning radius 1.7 meters, which can turn around in place. 5. Triple anti-water intrusion devices: waterproof cover, floating ball devices and liquid level sensors. 6. Design of two water injection ports: the large water injection port can achieve rapid water injection, and the smallone is suitable for places far away from the water source. 7. Lead-acid maintenance-free battery ensures continuous working hours.",
   },
 ];
