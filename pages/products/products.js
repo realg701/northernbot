8cfdb20b9c002c6ec61bc8e5b9ab711c2c9ff4b8
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-06-27 03:34:10 +0500

    uwu

--- pages/products/products.js
+++ pages/products/products.js
@@ -1,10 +1,14 @@
 import { data } from "./data.js";
 
-$("#startchange").ready(function () {
-  $("#startchange").append(
+const currentURL = window.location.href;
+const gitHubURL = currentURL.includes("github");
+const preFix = gitHubURL ? "/northernbot" : "";
+
+$("#products-container").ready(function () {
+  $("#products-container").append(
     data.map(
       (product) =>
-        `<a href=${product.link}><div class="card">
+        `<a href=${preFix + product.link}><div class="card">
           <img src=${product.image} alt="product image" style="width:100%">
           <div class="card-container">
             <h4><b>${product.title}</b></h4>
